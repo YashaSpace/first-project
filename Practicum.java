a9a1950d8967c394feea27eacecb8089c7c7a950
Yasha <yasha.yakovle@yandex.ru>
2024-07-22 13:58:25 +0300

    Создали HashMap для поиска столицы

--- Practicum.java
+++ Practicum.java
@@ -1,5 +1,9 @@
+import java.util.HashMap;
+
 public class Practicum {
     public static void main(String[] args) {
-        
+        HashMap<String, String> city = new HashMap<>();
+        city.put("Россия", "Москва");
+        System.out.println(city);
     }
 }
